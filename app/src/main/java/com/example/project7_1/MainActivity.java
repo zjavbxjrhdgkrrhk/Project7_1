32c11e0910838adc80fc1965159b8775ce61a190
JUNO SHIN <tlswnsgh0624@naver.com>
2023-05-15 20:06:50 +0900

    Initial commit

---
+++ app/src/main/java/com/example/project7_1/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.project7_1;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+    LinearLayout baseLayout;
+    Button button1;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        MenuInflater mInflater = getMenuInflater();
+        mInflater.inflate(R.menu.menu1, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.itemRed:
+                baseLayout.setBackgroundColor(Color.RED);
+                return true;
+            case R.id.itemGreen:
+                baseLayout.setBackgroundColor(Color.GREEN);
+                return true;
+            case R.id.itemBlue:
+                baseLayout.setBackgroundColor(Color.BLUE);
+                return true;
+            case R.id.subRotate:
+                button1.setRotation(45);
+                return true;
+            case R.id.subSize:
+                button1.setScaleX(2);
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("배경색 바꾸기");
+        baseLayout = findViewById(R.id.baseLayout);
+        button1 = findViewById(R.id.button1);
+    }
+}
